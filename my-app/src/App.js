545592047289da4df5f3d094f0d1745fa85d6e41
soni653 <dave.family653@outlook.com>
2020-04-13 17:20:32 -0400

    done with stretch

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UserCard from './components/UserCard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <UserCard/>
+    </div>
+  );
+}
+
+export default App;
